3d33b629579a2ab2beabd72b570c4657f945c4d3
Hayyan67 <humairabaig81@gmail.com>
2024-10-19 14:51:10 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+a={2,5,7,12,3,23,24,65,42,2,3}
+print('set a' ,a)
+a.add(67)
+print('updated set a' ,a)
+a.pop ()
+print('updated set a' ,a)
+b={2,5,'7',12,13,6,23,'65',42,2,3.2}
+print('updated set b' ,b)
